3adfea9057c9593abf2ae3169ddf5f132b3bdf96
xxwwyytt017 <xih017@ucsd.edu>
2020-10-02 20:58:06 +0800

    Add a feature in which user can tap a poster in the collection view to see a detail screen of that movie

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -51,15 +51,20 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        // find the selected movie
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        // pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
     }
-    */
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movies.count
