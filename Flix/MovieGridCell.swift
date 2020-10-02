d1e73637ef4d5c576c039941d0c5aa8a296a0a30
xxwwyytt017 <xih017@ucsd.edu>
2020-10-02 14:26:04 +0800

    Flix - Part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Xiaowang Huang on 9/30/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
